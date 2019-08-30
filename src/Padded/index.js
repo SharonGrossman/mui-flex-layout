65f805938875c8efbb73626fe8158c0e65e71a78
Sharon Grossman <sharong1337@gmail.com>
2019-08-31 00:19:22 +0300

    feat(layout): added row,column,padded and general layout components

---
+++ src/Padded/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Box} from '@material-ui/core';
+
+export default ({children, padding = 1, ...rest}) => (
+    <Box p={padding} {...rest}>
+        {children}
+    </Box>
+);
---
+++ src/Row/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Box} from '@material-ui/core';
+
+export default ({children, ...rest}) => (
+    <Box display={'flex'} flexDirection={'row'} {...rest}>
+        {children}
+    </Box>
+);
